e9967aff15a712d8b9ba391214657a976c69c59c
Davyd Bredykhin <correodemimobil@gmail.com>
2022-05-24 23:08:15 +0200

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './styles/App.css'
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+   const [posts, setPosts] = useState([
+      {id: 1, title: 'x', body: 'c'},
+      {id: 2, title: 'y', body: 'a'},
+      {id: 3, title: 'w', body: 'd'}
+   ]);
+   const [selectedSort, setSelectedSort] = useState("")
+
+   function createPost(newPost) {
+      setPosts([...posts, newPost])
+   }
+
+   function removePost(post) {
+      setPosts(posts.filter(p => p.id !== post.id))
+   }
+
+   function sortPosts(sort) {
+      setSelectedSort(sort);
+      setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+   }
+
+   return (
+     <div className="App">
+        <PostForm create={createPost}/>
+        <hr style={{margin: '15px 0'}} />
+        <div>
+           <MySelect
+              value={selectedSort}
+              onChange={sortPosts}
+              defaultValue="Sorted By:"
+              options={[
+                 {value: 'title', name: 'by name'},
+                 {value: 'body', name: 'by description'},
+
+              ]}
+           />
+        </div>
+        {posts.length
+           ?
+           <PostList remove={removePost} posts={posts} title={"Post List 1"}/>
+           :
+           <h1 style={{textAlign: "center"}} >
+              Posts not found
+           </h1>
+        }
+     </div>
   );
-}
+};
 
 export default App;
+
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import MyButton from "./UI/button/MyButton";
 
-const PostItem = () => {
+const PostItem = (props) => {
    return (
-      <div>
-
+      <div className="post">
+         <div className="post__content">
+            <strong>{props.number}. {props.post.title}</strong>
+            <div>{props.post.body}</div>
+         </div>
+         <div className="post__btns">
+            <MyButton onClick={() => props.remove(props.post)}>Delete</MyButton>
+         </div>
       </div>
    );
 };
